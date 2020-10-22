d72e1f1a51e8df3cd6e2f8e4b372ff9303d69422
María Agostina Costa <70040471+magostinacosta@users.noreply.github.com>
2020-10-22 01:03:29 -0300

    Add files via upload

---
+++ bucles.js
@@ -0,0 +1,21 @@
+let nueve = 9
+let diez = 10
+let once = 11
+for (let n = 1; n <= 9; n++) {
+    if (n == 1) {
+        document.write(`<h2>Tabla del 11</h2>`)
+    }
+    document.write(`<p>${once}x${n}=${once*n}</p>`)
+}
+for (let n = 1; n <= 9; n++) {
+    if (n == 1) {
+        document.write(`<h2>Tabla del 10</h2>`)
+    }
+    document.write(`<p>${diez}x${n}=${diez*n}</p>`)
+}
+for (let n = 1; n <= 9; n++) {
+    if (n == 1) {
+        document.write(`<h2>Tabla del 9</h2>`)
+    }
+    document.write(`<p>${nueve}x${n}=${nueve*n}</p>`)
+}
